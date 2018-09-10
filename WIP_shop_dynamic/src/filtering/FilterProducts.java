b20b8892288105936a7a973bf44804bc3ee07f48
Thomas <thomas.j.lissowski@gmail.com>
2018-09-10 11:49:15 +0200

    Filtering now working - added search function
    removed second servlet call
    search function searches for a topic with a similar name or a teacher
    with a similar name

--- WIP_shop_dynamic/src/filtering/FilterProducts.java
+++ WIP_shop_dynamic/src/filtering/FilterProducts.java
@@ -2,82 +2,105 @@ package filtering;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import functions.DBFunctions;
+import functions.Functions_Std;
 
 public class FilterProducts {
 	private ResultSet rs = null;
+	private String sqlQuery = "select * from courses";
 	
-	public FilterProducts(String subject, String topic, int pricePerHour, String capacity, String frequency, String durationPerMeeting, String addressID){
+	public FilterProducts(String subject, String topic, int pricePerHour, int capacity, String frequency, int durationPerMeeting, String addressID, String searchText){
 		StringBuilder sBuilder = new StringBuilder();
-		try {
-			Boolean hasAtLeastOneConditionBefore = false;
-			sBuilder.append("select * from courses WHERE ");
-			if (subject != null) {
-				if (hasAtLeastOneConditionBefore) {
-					sBuilder.append(" AND ");						
-				}
-				sBuilder.append("subject = '" + subject + "'");	
-				hasAtLeastOneConditionBefore = true;
+		Boolean hasAtLeastOneConditionBefore = false;
+		
+		sBuilder.append(" WHERE ");
+		if(searchText != null){
+			if (hasAtLeastOneConditionBefore) {
+				sBuilder.append(" AND ");						
 			}
-
-			if (topic != null) {
-				if (hasAtLeastOneConditionBefore) {
-					sBuilder.append(" AND ");						
-				}
-				sBuilder.append("topic = '" + topic + "'");	
-				hasAtLeastOneConditionBefore = true;
+			sBuilder.append("(topic LIKE('%" + searchText + "%') OR userID IN(SELECT userID FROM user WHERE username LIKE('%" + searchText + "%')))");
+			hasAtLeastOneConditionBefore = true;
+		}
+		
+		if (subject != null) {
+			if (hasAtLeastOneConditionBefore) {
+				sBuilder.append(" AND ");						
 			}
-			
-			if (pricePerHour > 0) {
-				if (hasAtLeastOneConditionBefore) {
-					sBuilder.append(" AND ");						
-				}
-				sBuilder.append("pricePerHour = " + pricePerHour);	
-				hasAtLeastOneConditionBefore = true;
+			sBuilder.append("LOWER(subject) = '" + subject + "'");	
+			hasAtLeastOneConditionBefore = true;
+		}
+		
+		if (topic != null) {
+			if (hasAtLeastOneConditionBefore) {
+				sBuilder.append(" AND ");						
 			}
-			
-			if (capacity != null) {
-				if (hasAtLeastOneConditionBefore) {
-					sBuilder.append(" AND ");						
-				}
-				sBuilder.append("capacity = '" + capacity + "'");	
-				hasAtLeastOneConditionBefore = true;
+			sBuilder.append("LOWER(topic) = '" + topic + "'");	
+			hasAtLeastOneConditionBefore = true;
+		}
+		
+		if (pricePerHour > 0) {
+			if (hasAtLeastOneConditionBefore) {
+				sBuilder.append(" AND ");						
 			}
-			
-			if (frequency != null) {
-				if (hasAtLeastOneConditionBefore) {
-					sBuilder.append(" AND ");						
-				}
-				sBuilder.append("frequency = '" + frequency + "'");	
-				hasAtLeastOneConditionBefore = true;
+			sBuilder.append("pricePerHour = " + pricePerHour);	
+			hasAtLeastOneConditionBefore = true;
+		}
+		
+		if (capacity > 0) {
+			if (hasAtLeastOneConditionBefore) {
+				sBuilder.append(" AND ");						
 			}
-			
-			if (durationPerMeeting != null) {
-				if (hasAtLeastOneConditionBefore) {
-					sBuilder.append(" AND ");						
-				}
-				sBuilder.append("durationPerMeeting = '" + durationPerMeeting + "'");	
-				hasAtLeastOneConditionBefore = true;
+			sBuilder.append("capacity = '" + capacity + "'");	
+			hasAtLeastOneConditionBefore = true;
+		}
+		
+		if (frequency != null) {
+			if (hasAtLeastOneConditionBefore) {
+				sBuilder.append(" AND ");						
 			}
-			
-			if (addressID != null) {
-				if (hasAtLeastOneConditionBefore) {
-					sBuilder.append(" AND ");						
-				}
-				sBuilder.append("addressID = '" + addressID + "'");	
-				hasAtLeastOneConditionBefore = true;
+			sBuilder.append("LOWER(frequency) = '" + frequency + "'");	
+			hasAtLeastOneConditionBefore = true;
+		}
+		
+		if (durationPerMeeting > 0) {
+			if (hasAtLeastOneConditionBefore) {
+				sBuilder.append(" AND ");						
 			}
-			
-			if(hasAtLeastOneConditionBefore){
-				rs = DBFunctions.Execute(sBuilder.toString());				
+			sBuilder.append("durationPerMeeting = '" + durationPerMeeting + "'");	
+			hasAtLeastOneConditionBefore = true;
+		}
+		
+		if (addressID != null) {
+			if (hasAtLeastOneConditionBefore) {
+				sBuilder.append(" AND ");						
 			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+			sBuilder.append("addressID = '" + addressID + "'");	
+			hasAtLeastOneConditionBefore = true;
+		}
+		
+		if(hasAtLeastOneConditionBefore){
+			sqlQuery += sBuilder.toString() + ";";
+		} 
+	}
+	
+	public FilterProducts(String sqlQuery){
+		if(!Functions_Std.isStringNullOrEmpty(sqlQuery)){
+			this.sqlQuery = sqlQuery;
 		}
 	}
 	
 	public ResultSet GetFilteredProducts (){
+		try {
+			if(rs == null){
+				rs = DBFunctions.Execute(sqlQuery);				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return rs;
+	}
+	
+	public String getSqlQuery(){
+		return sqlQuery;
 	}
 }
